a2cf6e8c02bd625c59f68dd089d52c68a70ec338
snowinjelly <broadwellconnor@gmail.com>
2024-06-15 14:56:58 -0500

    getmap

--- src/modules/battleship.js
+++ src/modules/battleship.js
@@ -1,17 +1,80 @@
-const Ship = () => {};
+const Ship = (size) => {
+  const shipSize = size;
+  let timesHit = 0;
+  let sunk = false;
+  let coordinates;
+
+  function getSize() {
+    return shipSize;
+  }
+
+  function hit() {
+    timesHit += 1;
+  }
+
+  function isSunk() {
+    if (timesHit === shipSize) {
+      sunk = true;
+    }
+    return sunk;
+  }
+
+  function getCoordinates() {
+    return coordinates;
+  }
+
+  function setCoordinates(cords) {
+    coordinates = cords;
+  }
+
+  return { getSize, isSunk, hit, getCoordinates, setCoordinates };
+};
 
 const Gameboard = () => {
-  let size = 10;
+  const size = 10;
 
-  function setSize(numSize) {
-    size = numSize;
+  function getColMarker(yPos) {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const arr = alphabet.split("");
+    return arr[yPos];
   }
+
+  function init() {
+    const arr = [];
+    for (let i = 0; i < size; i += 1) {
+      for (let j = 0; j < size; j += 1) {
+        arr.push({ x: i, y: j, row: i + 1, col: getColMarker(j) });
+      }
+    }
+    return arr;
+  }
+
+  function getMap() {
+    const map = init();
+    const dictionary = {
+      columns: {},
+      rows: {},
+    };
+    for (let i = 0; i < size; i += 1) {
+      const arrColumns = map.filter((value) => value.col === getColMarker(i));
+      dictionary.columns[getColMarker(i)] = arrColumns;
+
+      const arrRows = map.filter((value) => value.row === i + 1);
+      dictionary.rows[i + 1] = arrRows;
+    }
+
+    return dictionary;
+  }
+
+  const map = getMap();
 
   function getSize() {
     return size;
   }
 
-  return { setSize, getSize };
+  function placeShip(coords) {}
+
+  return { getSize };
 };
 
 const Player = () => {
@@ -34,6 +97,7 @@ const Game = () => {
 
   function getPlayableShips() {
     const total = Object.values(playableShips).reduce(
+      // eslint-disable-next-line no-return-assign, no-param-reassign
       (prev, cur) => (cur += prev)
     );
 
