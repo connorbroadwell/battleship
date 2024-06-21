0823176d4b850b564c609c40b6c8a94608907a0f
snowinjelly <broadwellconnor@gmail.com>
2024-06-21 13:48:04 -0500

    form styling

--- src/modules/main.js
+++ src/modules/main.js
@@ -9,7 +9,7 @@ import {
   renderGameStart,
 } from "./DOM";
 
-setBodyInnerHTML(renderGameStart());
+// setBodyInnerHTML(renderGameStart());
 const initHTML = getBodyInnerHTML();
 
 const game = Game();
